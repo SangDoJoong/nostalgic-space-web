a2c1bbc738ba9924678bfcc7763b1ca8bf01f93b
SangUk <psu9808@gmail.com>
2024-01-16 22:09:00 +0900

    [Feature] Show google map (#6)
    * feat: Render Google Map
    
    * fix: Apply ts

--- src/routes/About.tsx
+++ src/routes/About.tsx
@@ -1,8 +1,49 @@
+import React from 'react';
+import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
+import { useMemo } from 'react';
+
+const containerStyle = {
+  width: '100%',
+  height: '100vh',
+};
+
+const OPTIONS = {
+  minZoom: 4,
+  maxZoom: 18,
+};
+
 function About() {
-  return (
-    <div>
-      <h2>Home</h2>
-    </div>
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_KEY ?? '',
+  });
+  const center = useMemo(() => ({ lat: 37, lng: 127 }), []);
+
+  const [map, setMap] = React.useState(null);
+
+  const onLoad = React.useCallback(function callback(map: any) {
+    const bounds = new window.google.maps.LatLngBounds(center);
+    map.fitBounds(bounds);
+
+    setMap(map);
+  }, []);
+
+  const onUnmount = React.useCallback(function callback(map: any) {
+    setMap(null);
+  }, []);
+
+  return isLoaded ? (
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      onLoad={onLoad}
+      onUnmount={onUnmount}
+      options={OPTIONS}
+    >
+      <Marker position={center}></Marker>
+    </GoogleMap>
+  ) : (
+    <></>
   );
 }
 
