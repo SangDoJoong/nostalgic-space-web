313184ff49c916752d522b1febd5d553e48ddbaf
SangUk <psu9808@gmail.com>
2024-03-31 21:48:49 +0900

    [Feature] Map View에 헤더 추가 (#9)
    * feat: Modify view page
    
    * chore: Update ts config

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './routes/Home';
 import About from './routes/About';
 import SignIn from './routes/SignIn';
-import Copyright from './Components/Copyright';
-import * as React from 'react';
-import { RecoilRoot } from 'recoil';
 import SignUp from './routes/SignUp';
 import ProtectedRoute from './routes/ProtectedRoute';
+import Main from 'src/pages/main';
+import { RecoilRoot } from 'recoil';
+import * as React from 'react';
 
 function App() {
   return (
@@ -16,14 +16,13 @@ function App() {
         <Routes>
           <Route element={<ProtectedRoute />}>
             <Route path="/" element={<Home />} />
+            <Route path="/view" element={<Main />} />
             <Route path="/about" element={<About />} />
           </Route>
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="*" element={<h1>Not Found</h1>} />
         </Routes>
-
-        <Copyright sx={{ mt: 8, mb: 4 }} />
       </div>
     </RecoilRoot>
   );
