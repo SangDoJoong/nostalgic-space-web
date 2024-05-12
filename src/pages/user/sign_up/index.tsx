c4a6da46fb54b76b9562e7d8cfdf1a72fc0e7ef6
YuSunjo <tnswh2023@gmail.com>
2024-05-12 16:35:37 +0900

    feat(refactoring): 상대경로에서 절대경로로 수정

--- src/pages/user/sign_up/index.tsx
+++ src/pages/user/sign_up/index.tsx
@@ -13,7 +13,7 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { useSetRecoilState } from 'recoil';
-import { signup, userState, isLoggedInState } from '../../../recoil/atoms/auth';
+import { signup, userState, isLoggedInState } from 'src/recoil/atoms/auth';
 import { useNavigate } from 'react-router-dom';
 
 const defaultTheme = createTheme();
@@ -114,7 +114,7 @@ export default function SignUp() {
             </Button>
             <Grid container justifyContent="flex-end">
               <Grid item>
-                <Link href="/sign-in" variant="body2">
+                <Link href="/user/sign-in" variant="body2">
                   Already have an account? Sign in
                 </Link>
               </Grid>
