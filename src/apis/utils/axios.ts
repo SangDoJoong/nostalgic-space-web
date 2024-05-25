e48b08ac3d29aa01999d800c4913047d75d03267
YuSunjo <tnswh2023@gmail.com>
2024-05-25 18:26:51 +0900

    feat(reabse): feature/usenostalgic-api-server reabse

--- src/apis/utils/axios.ts
+++ src/apis/utils/axios.ts
@@ -5,7 +5,10 @@ import { WebPageBaseURL } from 'src/constants/constants';
   API client instance connects to the Nostalgic API Server.
 */
 export const axiosAPIClient: AxiosInstance = Axios.create({
-  baseURL: WebPageBaseURL,
+  baseURL: `${WebPageBaseURL}/api`,
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
 /**
@@ -13,4 +16,21 @@ export const axiosAPIClient: AxiosInstance = Axios.create({
   Helps to call APIs that use the authentication.
   This instance requires JWT for the authentication.
 */
-export const authAxiosAPIClient: AxiosInstance = Axios.create({});
+export const authAxiosAPIClient: AxiosInstance = Axios.create({
+  baseURL: `${WebPageBaseURL}/api`,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+authAxiosAPIClient.interceptors.request.use(
+  config => {
+    const accessToken = !!localStorage.getItem('token'); // Get JWT from the local storage.
+
+    config.headers['Authorization'] = `Bearer ${accessToken}`; // Set JWT in headers before executing API call.
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  },
+);
--- src/inferfaces/user.interface.ts
+++ src/inferfaces/user.interface.ts
@@ -1,6 +1,5 @@
-interface user {
+export interface User {
   username: string;
-  email: string;
   password?: string;
   token?: string;
 }
