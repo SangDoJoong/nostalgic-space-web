ed1dd51ad0cadbda116aaa04b65ce8ce8fabbfaa
YuSunjo <tnswh2023@gmail.com>
2024-05-25 19:13:19 +0900

    feat(feature): - 회원가입 구현

--- src/apis/User/index.ts
+++ src/apis/User/index.ts
@@ -21,3 +21,29 @@ export async function signInUser(username: string, password: string) {
     return { username: '', access_token: '' };
   }
 }
+
+export async function signUpUser(
+  name: string,
+  password1: string,
+  password2: string,
+) {
+  try {
+    const response = await axiosAPIClient.post('/user/create', {
+      name,
+      password1,
+      password2,
+    });
+    // Parse result from data
+    const { status_code, detail } = response.data;
+
+    if (status_code !== 200) {
+      new Error(detail);
+    } else {
+      return { status_code, detail };
+    }
+  } catch (err) {
+    // TODO: Error handling
+    console.log(err);
+    return { username: '', access_token: '' };
+  }
+}
